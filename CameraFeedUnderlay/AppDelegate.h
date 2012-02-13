1c3451d7a7dce084fc0efed7b263c278bcce578e
Daniel Shein <shein@loftdev.com>
2012-02-13 17:17:53 +0200

    Initial Commit

---
+++ CameraFeedUnderlay/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CameraFeedUnderlay
+//
+//  Created by Daniel Shein on 2/13/12.
+//  Copyright (c) 2012 LoFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CameraFeedUnderlay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraFeedUnderlay
+//
+//  Created by Daniel Shein on 2/13/12.
+//  Copyright (c) 2012 LoFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
